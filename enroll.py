dfdd596aff1ab325d4017cab43eb8944bd229d1a
Marlon Liu <Marlon.Liu@ibm.com>
2022-04-01 23:14:46 -0400

    using switch frame instead of inner frame url

--- enroll.py
+++ enroll.py
@@ -17,30 +17,20 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 LOGIN_URL = "https://epprd.mcmaster.ca/psp/prepprd/?cmd=login"
-ENROLL_URL = "https://csprd.mcmaster.ca/psc/prcsprd/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_CART.GBL?" \
-             "Page=SSR_SSENRL_CART&Action=A&ACAD_CAREER=UGRD&EMPLID={STUDENTNUM}&INSTITUTION=MCMST&STRM={TERMNUM}"
+ENROLL_URL = "https://epprd.mcmaster.ca/psp/prepprd/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL"
 
 parser = argparse.ArgumentParser(description="Mosaic Auto Enroller")
 parser.add_argument('-u', '--username', help='Mosaic Username', required=True)
 parser.add_argument('-p', '--password', help='Mosaic Password', required=True)
-parser.add_argument('-n', '--student_num', help="Student Number", required=True)
 parser.add_argument('-c', '--course_id', help="Course ID", required=True)
-parser.add_argument('-t', '--term', help="The term course is in", required=True)
+parser.add_argument('-t', '--term', help="The term course is in, Replace space between with underscore", required=True)
 args = parser.parse_args()
 USERNAME = args.username
-STUDENT_NUMBER = args.student_num
 PASSWORD = args.password
 TERM = args.term
 COURSE_ID = args.course_id
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-def scraper(url):
-    options = Options()
-    # options.add_argument("--headless")
-    driver.get(url)
-    time.sleep(2)
-    return driver.page_source
 
 
 def login(url):
@@ -62,29 +52,47 @@ def login(url):
         driver.quit()
 
 
-def add_to_cart(url, course_id, term):
-    TERM_MAP = {
-        "2022 Winter": 2221,
-        "2022 Spring/Summer": 2225
-    }
+def add_course_to_cart(url, course_id, input_term):
 
-    # get term ID and complete enroll url
-    term_num = TERM_MAP[term]
-    enroll_url = url.format(TERMNUM=term_num, STUDENTNUM = STUDENT_NUMBER)
-    driver.get(enroll_url)
+    driver.get(url)
+    iframe = driver.find_element(By.ID, "ptifrmtgtframe")
+    driver.switch_to.frame(iframe)
+    time.sleep(1)
+
+    driver.find_element(By.XPATH, "//a[contains(@name, 'DERIVED_SSS_SCR_SSS_LINK_ANCHOR3')]").click()
+    term_table = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located(((By.XPATH, "//tr[contains(@id, 'trSSR_DUMMY_RECV1')]"))))
+    term_str = " ".join(input_term.split('_'))
+    logger.info("Looking for term: {}".format(term_str))
+    for idx, term in enumerate(term_table):
+        try:
+            term.find_element(By.XPATH,  "//span[text()=\"{term}\"]".format(term=term_str))
+            time.sleep(1)
+            logger.info("Term found")
+            term.find_element(By.XPATH, "//input[contains(@title, 'Select this row')]").click()
+            break
+        except:
+            if idx == len(term_table) - 1:
+                logger.error("Provided term not found, exiting")
+                driver.quit()
+                exit()
+            continue
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//input[contains(@value, 'Continue')]"))).click()
 
     # search class
-    class_input = driver.find_element(By.ID, "DERIVED_REGFRM1_CLASS_NBR")
+    logger.info("Searching class")
+    class_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "DERIVED_REGFRM1_CLASS_NBR")))
     class_input.send_keys(course_id)
     time.sleep(1)
 
     search_btn = driver.find_element(By.ID, "DERIVED_REGFRM1_SSR_PB_ADDTOLIST2$9$")
     search_btn.click()
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@id='win0divDERIVED_CLS_DTL_DESCR50']")))
-    logger.info("succeed getting to enroll page")
+    logger.info("Class found")
 
     # check if any labs or tutorials needs to be selected
-    if EC.presence_of_element_located((By.XPATH, '//td[text()="Select Tutorial section (Required):" or text()="Select Laboratory section (Required):"]')):
+    if WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//td[text()="Select Tutorial section (Required):" or text()="Select Laboratory section (Required):"]'))):
         all_sections = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//tr[contains(@id, 'trSSR_CLS_TBL_R1')]")))
         for idx, section in enumerate(all_sections):
             try:
@@ -97,7 +105,7 @@ def add_to_cart(url, course_id, term):
                 time.sleep(1)
                 break
             except Exception as e:
-                if idx == len(all_sections):
+                if idx == len(all_sections) - 1:
                     logger.error("No available lab/tut section, quiting")
                     driver.quit()
                 continue
@@ -118,10 +126,5 @@ def add_to_cart(url, course_id, term):
 
 
 if __name__ == "__main__":
-    # html_str = scraper(url)
-    # content = HTML(html=html_str)
-    # user_input = content.find(".userid", first=True)
-    # pw_input = content.find(".pwd", first=True)
-
     login(LOGIN_URL)
-    print(add_to_cart(ENROLL_URL, COURSE_ID, TERM))
+    logger.info(add_course_to_cart(ENROLL_URL, COURSE_ID, TERM))
